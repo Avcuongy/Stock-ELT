741bacc094f5a0f3cfb7a79bd70a2b84adcf5ad4
Avcuongy <dangxuancuong123456789@gmail.com>
2026-06-19 15:42:33 +0700

    um

--- airflow/dags/elt.py
+++ airflow/dags/elt.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.providers.standard.sensors.external_task import ExternalTaskSensor
 
 PROJECT_PATH = "/path/to/your/stock-elt"
 PYTHON_EXEC = f"{PROJECT_PATH}/.venv/Scripts/python"
@@ -16,22 +15,12 @@ default_args = {
 with DAG(
     dag_id="elt",
     default_args=default_args,
-    description="Extract from Source, Load and Transform in DuckDB",
-    schedule="0 1 * * *",
+    description="ELT",
+    schedule="*/30 * * * *",
     start_date=datetime(2026, 6, 14),
     catchup=False,
     tags=["warehouse", "elt"],
 ) as dag:
-
-    wait_for_backend = ExternalTaskSensor(
-        task_id="wait_for_backend_source",
-        external_dag_id="backend_etl",
-        external_task_id="backend_load",
-        allowed_states=["success"],
-        execution_delta=timedelta(seconds=0),
-        timeout=3600,
-        poke_interval=60,
-    )
 
     task_extract = BashOperator(
         task_id="elt_extract",
@@ -48,4 +37,4 @@ with DAG(
         bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/elt/transform.py",
     )
 
-    wait_for_backend >> task_extract >> task_load >> task_transform
+    task_extract >> task_load >> task_transform
--- src/utils/setup_folder.py
+++ src/utils/setup_folder.py
@@ -7,7 +7,12 @@ from utils.logger import get_logger
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_ROOT = PROJECT_ROOT / "data"
-LOGS_DIR = PROJECT_ROOT / "logs" / "config.log"
+LOGS_DIR = PROJECT_ROOT / "logs"
+LOGS_FILE = [
+    "config.log",
+    "backend.log",
+    "elt.log",
+]
 DATA_SUBFOLDERS = [
     # completed layer
     "completed/db",
@@ -34,10 +39,19 @@ def _ensure_data_folders() -> None:
         folder.mkdir(parents=True, exist_ok=True)
 
 
+def _ensure_logs_folder() -> None:
+    LOGS_DIR.mkdir(exist_ok=True)
+    for log_file in LOGS_FILE:
+        log_path = LOGS_DIR / log_file
+        if not log_path.exists():
+            log_path.touch()
+
+
 def setup_folder() -> None:
-    logger.info(f"[Config] Make data folders at: {DATA_ROOT}")
+    logger.info("[Config] Setup data folders at %s", DATA_ROOT)
     _ensure_data_folders()
-    logger.info("[Config] Data folder structure is ready")
+    logger.info("[Config] Setup logs folder at %s", LOGS_DIR)
+    _ensure_logs_folder()
 
 
 if __name__ == "__main__":
