0087e0f4ce674911c893925024824f51310549d8
Avcuongy <dangxuancuong123456789@gmail.com>
2026-06-22 14:03:41 +0700

    airflow

--- airflow/dags/backend.py
+++ airflow/dags/backend.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.sdk import DAG
+from airflow import DAG
 
 PROJECT_PATH = "/opt/airflow/project"
-PYTHON_EXEC = "python"
+PYTHON_EXEC = f"export PYTHONPATH={PROJECT_PATH} && python"
 
 default_args = {
     "owner": "airflow",
@@ -17,24 +17,57 @@ with DAG(
     default_args=default_args,
     description="Crawl API and Load into MySQL Source Database",
     schedule="0 1 1 * *",
-    start_date=datetime(2026, 6, 14),
+    start_date=datetime(2026, 6, 20),
     catchup=False,
     tags=["source", "backend", "etl"],
 ) as dag:
 
-    task_extract = BashOperator(
-        task_id="backend_extract",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/backend/extract.py",
+    task_extract_1 = BashOperator(
+        task_id="crawl_companies",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/extract/crawl_companies.py",
     )
 
-    task_transform = BashOperator(
-        task_id="backend_transform",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/backend/transform.py",
+    task_extract_2 = BashOperator(
+        task_id="crawl_markets",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/extract/crawl_markets.py",
     )
 
-    task_load = BashOperator(
-        task_id="backend_load",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/backend/load.py",
+    task_transform_1 = BashOperator(
+        task_id="transform_others_1",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/transform/transform_others_1.py",
     )
 
-    task_extract >> task_transform >> task_load
+    task_transform_2 = BashOperator(
+        task_id="transform_exchanges_2",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/transform/transform_exchanges_2.py",
+    )
+
+    task_transform_3 = BashOperator(
+        task_id="transform_companies_3",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/transform/transform_companies_3.py",
+    )
+
+    task_load_1 = BashOperator(
+        task_id="load_others_1",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/load/load_db_others_1.py",
+    )
+
+    task_load_2 = BashOperator(
+        task_id="load_exchanges_2",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/load/load_db_exchanges_2.py",
+    )
+
+    task_load_3 = BashOperator(
+        task_id="load_companies_3",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/backend/load/load_db_companies_3.py",
+    )
+
+    (
+        [task_extract_1, task_extract_2]
+        >> task_transform_1
+        >> task_transform_2
+        >> task_transform_3
+        >> task_load_1
+        >> task_load_2
+        >> task_load_3
+    )
--- airflow/dags/elt.py
+++ airflow/dags/elt.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
 PROJECT_PATH = "/opt/airflow/project"
-PYTHON_EXEC = "python"
+PYTHON_EXEC = f"export PYTHONPATH={PROJECT_PATH} && python"
 
 default_args = {
     "owner": "airflow",
@@ -16,25 +16,46 @@ with DAG(
     dag_id="elt_main",
     default_args=default_args,
     description="ELT",
-    schedule="*/50 * * * *",
+    schedule="0 1 * * *",
     start_date=datetime(2026, 6, 14),
     catchup=False,
     tags=["warehouse", "elt"],
 ) as dag:
 
     task_extract = BashOperator(
-        task_id="elt_extract",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/elt/extract.py",
+        task_id="crawl_ohlcs",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/extract/crawl_ohlcs.py",
     )
 
-    task_load = BashOperator(
-        task_id="elt_load",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/elt/load.py",
+    task_load_1 = BashOperator(
+        task_id="convert_api_to_parquet",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/load/convert_api_to_parquet.py",
     )
 
-    task_transform = BashOperator(
-        task_id="elt_transform",
-        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} scripts/elt/transform.py",
+    task_load_2 = BashOperator(
+        task_id="convert_db_to_parquet",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/load/convert_db_to_parquet.py",
     )
 
-    task_extract >> task_load >> task_transform
+    task_load_3 = BashOperator(
+        task_id="load_to_hdfs",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/load/load_to_hdfs.py",
+    )
+
+    task_transform_1 = BashOperator(
+        task_id="transform_1",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/transform/transform_1.py",
+    )
+
+    task_transform_2 = BashOperator(
+        task_id="transform_2",
+        bash_command=f"cd {PROJECT_PATH} && {PYTHON_EXEC} src/elt/transform/transform_2.py",
+    )
+
+    (
+        task_extract
+        >> [task_load_1, task_load_2]
+        >> task_load_3
+        >> task_transform_1
+        >> task_transform_2
+    )
